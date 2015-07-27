2923cc4bf5ab6e768e05faf2ad8f60dcc29049fd
debarund <debarund@BLR-DEBARUND.vmware.com>
2015-07-27 11:12:15 +0530

    prescription upload rest service

--- RESTem/src/main/java/com/pridet/restservices/BaseRestService.java
+++ RESTem/src/main/java/com/pridet/restservices/BaseRestService.java
@@ -1,5 +1,8 @@
 package com.pridet.restservices;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 import com.pridet.constant.ServiceTypesConstants;
 import com.pridet.factory.ServiceFactory;
 import com.pridet.services.IServices;
@@ -7,12 +10,22 @@ import com.pridet.services.IServices;
 public class BaseRestService {
 
 	protected IServices deliveredService;
+	protected Properties prop = new Properties();
+	private InputStream input = null;
 	
 	public BaseRestService(ServiceTypesConstants type) {
 		deliveredService=ServiceFactory.getService(type);
+		try {
+			input = BaseRestService.class.getClassLoader().getResourceAsStream("config.properties");
+			prop.load(input);
+		} catch (Exception e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}
 	}
 	
 	public BaseRestService() {
+		
 	}
 	
 }
